6881b891f3749690d86b9ac4620cfb3f540c33af
ryanDMaule <davidmaule98@gmail.com>
2025-01-28 21:54:55 +0000

    Valid Palindrome #125
    - Unit tested and passed

---
+++ app/src/main/java/com/mauleCo/leetcode/ValidPalindrome.kt
@@ -0,0 +1,32 @@
+package com.mauleCo.leetcode
+
+import kotlin.math.floor
+
+class ValidPalindrome {
+
+    /**
+     * DESCRIPTION
+     *
+     * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+     * and removing all non-alphanumeric characters, it reads the same forward and backward.
+     * Alphanumeric characters include letters and numbers.
+     *
+     * Given a string s, return true if it is a palindrome, or false otherwise.
+     */
+
+    private fun cleanString(input: String): String {
+        return input.lowercase().filter { it.isLetterOrDigit() }
+    }
+
+    fun isPalindrome(s: String): Boolean {
+//        val cs = cleanString(s)
+        val cs = s.lowercase().filter { it.isLetterOrDigit() }
+        for(i in 0 until floor((cs.length/2).toDouble()).toInt()){
+            if (cs[i] != cs[cs.length-i-1]){
+                return false
+            }
+        }
+        return true
+    }
+
+}
---
+++ app/src/test/java/com/mauleCo/leetcode/ValidPalindromeTest.kt
@@ -0,0 +1,42 @@
+package com.mauleCo.leetcode
+
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class ValidPalindromeTest {
+
+    // region constants
+
+    private val EXAMPLE_1 = "A man, a plan, a canal: Panama"
+    private val EXAMPLE_2 = "race a car"
+
+    // endregion constants
+
+    // region helper fields
+
+    // endregion helper fields
+
+    @Suppress("PrivatePropertyName")
+    private lateinit var SUT: ValidPalindrome
+
+    @Before
+    fun setup() {
+        SUT = ValidPalindrome()
+    }
+
+    @Test
+    fun isPalindrome_palindromePassed_trueReturned() {
+        val result: Boolean = SUT.isPalindrome(EXAMPLE_1)
+        Assert.assertThat(result, CoreMatchers.`is`(true))
+    }
+
+    @Test
+    fun isPalindrome_nonPalindromePassed_falseReturned() {
+        val result: Boolean = SUT.isPalindrome(EXAMPLE_2)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+}
