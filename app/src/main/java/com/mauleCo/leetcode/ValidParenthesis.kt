468781e1c45651c9dce6c93072377d1b928d82bb
ryanDMaule <davidmaule98@gmail.com>
2025-01-28 21:01:48 +0000

    Valid parenthesis #20
    - Unit tested and passed

---
+++ app/src/main/java/com/mauleCo/leetcode/ValidParenthesis.kt
@@ -0,0 +1,60 @@
+package com.mauleCo.leetcode
+
+class ValidParenthesis {
+
+    /**
+     * DESCRIPTION
+     *
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if
+     * the input string is valid.
+     *
+     * An input string is valid if:
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     */
+
+    fun isValid(s: String): Boolean {
+        //if string length is not even its false
+        if (s.length % 2 != 0){
+            return false
+        }
+
+        val charArr = s.toCharArray()
+        val expectedCharList = mutableListOf<Char>()
+
+        for (i in charArr.indices){
+            //when we encounter a bracket opening, add the expected close bracket to the expected array
+            when(charArr[i]){
+                '(' -> {
+                    expectedCharList.add(0, ')')
+                }
+                '[' -> {
+                    expectedCharList.add(0,']')
+                }
+                '{' -> {
+                    expectedCharList.add(0,'}')
+                }
+
+                //when we encounter a close bracket check it the first of the expected array we create
+                ')',']','}' -> {
+                    //the try catches an instance where we encounter a closing bracket first and the array is empty
+                    return try {
+                        if (charArr[i] == expectedCharList[0]){
+                            expectedCharList.removeAt(0)
+                            continue
+                        } else {
+                            false
+                        }
+                    } catch (e : IndexOutOfBoundsException){
+                        false
+                    }
+                }
+            }
+        }
+
+        //if there is nothing left in the expected array then everything has been closed
+        return expectedCharList.size == 0
+    }
+
+}
---
+++ app/src/test/java/com/mauleCo/leetcode/ValidParenthesisTest.kt
@@ -0,0 +1,62 @@
+package com.mauleCo.leetcode
+
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class ValidParenthesisTest {
+
+    // region constants
+
+    private val EXAMPLE_1 : String = "())}"
+    private val EXAMPLE_2 : String = "()[]{}"
+    private val EXAMPLE_3 : String = "(])}"
+    private val EXAMPLE_4 : String = "{[]})}"
+    private val EXAMPLE_5 : String = "){)}"
+
+    // endregion constants
+
+    // region helper fields
+
+    // endregion helper fields
+
+    @Suppress("PrivatePropertyName")
+    private lateinit var SUT: ValidParenthesis
+
+    @Before
+    fun setup() {
+        SUT = ValidParenthesis()
+    }
+
+    @Test
+    fun isValid_extraClosingBrackets_false() {
+        val result: Boolean = SUT.isValid(EXAMPLE_1)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+    @Test
+    fun isValid_validString_true() {
+        val result: Boolean = SUT.isValid(EXAMPLE_2)
+        Assert.assertThat(result, CoreMatchers.`is`(true))
+    }
+
+    @Test
+    fun isValid_extraClosingBrackets2_false() {
+        val result: Boolean = SUT.isValid(EXAMPLE_3)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+    @Test
+    fun isValid_extraClosingBrackets3_false() {
+        val result: Boolean = SUT.isValid(EXAMPLE_4)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+    @Test
+    fun isValid_extraClosingBrackets4_false() {
+        val result: Boolean = SUT.isValid(EXAMPLE_5)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+}
